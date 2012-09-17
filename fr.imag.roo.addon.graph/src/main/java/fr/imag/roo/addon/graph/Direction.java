774d90f7842d60632068a134ece1cd19e72d58f4
Juan Castrejón <jccastrejon@gmail.com>
2012-09-17 22:07:41 +0200

    Initial import

---
+++ fr.imag.roo.addon.graph/src/main/java/fr/imag/roo/addon/graph/Direction.java
@@ -0,0 +1,10 @@
+package fr.imag.roo.addon.graph;
+
+/**
+ * 
+ * @author jccastrejon
+ * 
+ */
+public enum Direction {
+    INCOMING, OUTGOING
+}
---
+++ fr.imag.roo.addon.graph/src/main/java/fr/imag/roo/addon/graph/GraphOperations.java
@@ -0,0 +1,16 @@
+package fr.imag.roo.addon.graph;
+
+/**
+ * 
+ * @author jccastrejon
+ * 
+ */
+public interface GraphOperations {
+
+    /**
+     * 
+     * @param provider
+     * @param dataStoreLocation
+     */
+    public void graphSetup(final GraphProvider provider, final String dataStoreLocation);
+}
